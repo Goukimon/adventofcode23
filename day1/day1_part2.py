f4ff403ae07275af73bab30015a0f8ca64bfe249
Gouki <102922169+Goukimon@users.noreply.github.com>
2023-12-02 17:03:43 +0100

    Solution to part 2. Misread the instructions and thought sixteen and sixty were banned. The isBanned function stays here as a memorandum to read the manual

--- day1/day1_part2.py
+++ day1/day1_part2.py
@@ -1,9 +1,13 @@
 import string
+from tabnanny import check
 import time
 from enum import Enum
 
 ref = ['one','two','three','four','five','six','seven','eight','nine']
-unique_chrs = {'t', 'w', 's', 'i', 'x', 'g', 'u', 'h', 'n', 'e', 'f', 'o', 'v', 'r'}
+
+l1 = []
+for d in ref :
+    l1.append(d[0])
 
 def onlyDigits(s) :
         s = s.strip(string.ascii_lowercase)
@@ -14,74 +18,77 @@ def onlyDigits(s) :
             return str(a+b)
         else :
             return str(a+a)
+        
+def isBanned(check,i,s) :
+    if len(check) == 3 and len(s)-i >= 5 :
+        if ''+s[i+3]+s[i+4] == 'ty' :
+            return True
+    if len(check) == 3 and len(s)-i >= 7 :
+        if ''+s[i+3]+s[i+4]+s[i+5]+s[i+6] == 'teen' :
+            return True
+    if len(check) == 4 and len(s)-i >= 6 :
+        if ''+s[i+4]+s[i+5] == 'ty' :
+            return True
+    if len(check) == 4 and len(s)-i >= 8 :
+        if ''+s[i+4]+s[i+5]+s[i+6]+s[i+7] == 'teen' :
+            return True
+    if len(check) == 5 and len(s)-i >= 7 :
+        if ''+s[i+5]+s[i+6] == 'ty' :
+            return True
+    if len(check) == 5 and len(s)-i >= 9 :
+        if ''+s[i+5]+s[i+6]+s[i+7]+s[i+8] == 'teen' :
+            return True
+    return False
 
 def noMoreText(s) :
 
-    indices1 = s.index(ref[0])
-    indices2 = s.index(ref[1])
-    indices3 = s.index(ref[2])
-    indices4 = s.index(ref[3])
-    indices5 = s.index(ref[4])
-    indices6 = s.index(ref[5])
-    indices7 = s.index(ref[6])
-    indices8 = s.index(ref[7])
-    indices9 = s.index(ref[8])
+    s = list(s)
+    for i in range(len(s)) :
+            if s[i] in l1 :
+                if len(s)-i >= 3 :
+                    check = ''+s[i]+s[i+1]+s[i+2]
+                    if check == 'one' :
+                        s[i] = '1'
+                    if check == 'two' :
+                        s[i] = '2'
+                    if check == 'six' :
+                        s[i] = '6'
+                if len(s)-i >= 4 :
+                    check = ''+s[i]+s[i+1]+s[i+2]+s[i+3]
+                    if check == 'four' :
+                        s[i] = '4'
+                    if check == 'five' :
+                        s[i] = '5'
+                    if check == 'nine' :
+                        s[i] = '9'
+                if len(s)-i >= 5 :
+                    check = ''+s[i]+s[i+1]+s[i+2]+s[i+3]+s[i+4]
+                    if check == 'three' :
+                        s[i] = '3'
+                    if check == 'seven' :
+                        s[i] = '7'
+                    if check == 'eight' :
+                        s[i] = '8'
 
-    # Avancer dans la string.
-    # Si caractère hors de l'ensemble : delete le buffer.
-    # Si caractere dans l'ensemble : add to buffer, add next one.
-    # Quand buffersize == 3, commencer les checks pour remplacer
-    # Si on atteint les critiques, checks les x suivants pour voir si ty ou teen existe.
-
-    """
-    s = s.replace('one','1')
-    s = s.replace('two','2')
-    s = s.replace('three','3')
-    s = s.replace('five','5')
-    s = s.replace('four','4')
-    s = s.replace('six','6')
-    s = s.replace('seven','7')
-    s = s.replace('nine','9')
-    s = s.replace('eight','8')
-    s = s.replace('4ty','')
-    s = s.replace('4teen','')
-    s = s.replace('4ty','')
-    s = s.replace('4teen','')
-    s = s.replace('6ty','')
-    s = s.replace('6teen','')
-    s = s.replace('7ty','')
-    s = s.replace('7teen','')
-    s = s.replace('9ty','')
-    s = s.replace('9teen','')
-    s = s.replace('8y','')
-    s = s.replace('8een','')
-    """
+    s = ''.join(s)
     print(f"Post replace : {s}")
-    return 0
-    #return onlyDigits(s)
+    return onlyDigits(s)
 
 
 
 #------------------------------------------------------------------------------------------------------------------
-with open('smol.txt') as f :
+with open('input.txt') as f :
     lines = f.readlines()
 
 total = 0
-
-tout = ''.join(ref)
-allchars = set(list(tout))
-print(allchars)
 
 for line in lines :
     line = line.strip('\n')
     print(f"OG line : {line}")
     if ('one' in line) or ('two' in line) or ('three' in line) or ('four' in line) or ('five' in line) or ('six' in line) or ('seven' in line) or ('eight' in line) or ('nine' in line) :
-        total = total + int(noMoreText(line))
+        total += int(noMoreText(line))
     else : 
-        total = total + int(onlyDigits(line))
+        total += int(onlyDigits(line))
     print('-------------------------------------')
-    time.sleep(3)
 
 print(f"Sum of all calibration values, including text values : {total}")
-
-#53655 is too low
